c5ca597988cbe7a34f47e5e3e585d352fc637471
Diana Malyk <scintiamon@gmail.com>
2020-10-24 17:56:52 +0300

    Fix

--- test_adversarial_examples.py
+++ test_adversarial_examples.py
@@ -5,60 +5,70 @@ from torch.utils.data import DataLoader
 import models
 from models import MNIST_target_net
 
-use_cuda=True
-image_nc=1
-batch_size = 128
 
-gen_input_nc = image_nc
+if __name__ == "__main__":
+    
+    use_cuda=True
+    image_nc=1
+    batch_size = 128
 
-# Define what device we are using
-print("CUDA Available: ",torch.cuda.is_available())
-device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
+    gen_input_nc = image_nc
 
-# load the pretrained model
-pretrained_model = "./MNIST_target_model.pth"
-target_model = MNIST_target_net().to(device)
-target_model.load_state_dict(torch.load(pretrained_model))
-target_model.eval()
+    # Define what device we are using
+    print("CUDA Available: ",torch.cuda.is_available())
+    device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
 
-# load the generator of adversarial examples
-pretrained_generator_path = './models/netG_epoch_60.pth'
-pretrained_G = models.Generator(gen_input_nc, image_nc).to(device)
-pretrained_G.load_state_dict(torch.load(pretrained_generator_path))
-pretrained_G.eval()
+    # load the pretrained model
+    pretrained_model = "./MNIST_target_model.pth"
+    target_model = MNIST_target_net().to(device)
+    target_model.load_state_dict(torch.load(pretrained_model))
+    target_model.eval()
 
-# test adversarial examples in MNIST training dataset
-mnist_dataset = torchvision.datasets.MNIST('./dataset', train=True, transform=transforms.ToTensor(), download=True)
-train_dataloader = DataLoader(mnist_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
-num_correct = 0
-for i, data in enumerate(train_dataloader, 0):
-    test_img, test_label = data
-    test_img, test_label = test_img.to(device), test_label.to(device)
-    perturbation = pretrained_G(test_img)
-    perturbation = torch.clamp(perturbation, -0.3, 0.3)
-    adv_img = perturbation + test_img
-    adv_img = torch.clamp(adv_img, 0, 1)
-    pred_lab = torch.argmax(target_model(adv_img),1)
-    num_correct += torch.sum(pred_lab==test_label,0)
+    # load the generator of adversarial examples
+    pretrained_generator_path = './models/netG.pth.tar'
+    pretrained_G = models.Generator(gen_input_nc, image_nc).to(device)
+    pretrained_G.load_state_dict(torch.load(pretrained_generator_path))
+    pretrained_G.eval()
 
-print('MNIST training dataset:')
-print('num_correct: ', num_correct.item())
-print('accuracy of adv imgs in training set: %f\n'%(num_correct.item()/len(mnist_dataset)))
+    # test adversarial examples in MNIST training dataset
+    mnist_dataset = torchvision.datasets.MNIST('./dataset', train=True, transform=transforms.ToTensor(), download=True)
+    train_dataloader = DataLoader(mnist_dataset, batch_size=batch_size, shuffle=False, num_workers=1)
+    num_correct = 0
+    num_all = 0
+    for i, data in enumerate(train_dataloader, 0):
+        test_img, test_label = data
+        test_img, test_label = test_img.to(device), test_label.to(device)
+        perturbation = pretrained_G(test_img)
+        perturbation = torch.clamp(perturbation, -0.3, 0.3)
+        adv_img = perturbation + test_img
+        adv_img = torch.clamp(adv_img, 0, 1)
+        pred_lab = torch.argmax(target_model(adv_img),1)
+        num_all += len(test_label)
+        num_correct += torch.sum(pred_lab==test_label,0)
 
-# test adversarial examples in MNIST testing dataset
-mnist_dataset_test = torchvision.datasets.MNIST('./dataset', train=False, transform=transforms.ToTensor(), download=True)
-test_dataloader = DataLoader(mnist_dataset_test, batch_size=batch_size, shuffle=False, num_workers=1)
-num_correct = 0
-for i, data in enumerate(test_dataloader, 0):
-    test_img, test_label = data
-    test_img, test_label = test_img.to(device), test_label.to(device)
-    perturbation = pretrained_G(test_img)
-    perturbation = torch.clamp(perturbation, -0.3, 0.3)
-    adv_img = perturbation + test_img
-    adv_img = torch.clamp(adv_img, 0, 1)
-    pred_lab = torch.argmax(target_model(adv_img),1)
-    num_correct += torch.sum(pred_lab==test_label,0)
+    print('MNIST training dataset:')
+    print('num_examples: ', num_all)
+    print('num_correct: ', num_correct.item())
+    print('accuracy of adv imgs in training set: %f\n'%(num_correct.item()/num_all))
 
-print('num_correct: ', num_correct.item())
-print('accuracy of adv imgs in testing set: %f\n'%(num_correct.item()/len(mnist_dataset_test)))
+    # test adversarial examples in MNIST testing dataset
+    mnist_dataset_test = torchvision.datasets.MNIST('./dataset', train=False, transform=transforms.ToTensor(), download=True)
+    test_dataloader = DataLoader(mnist_dataset_test, batch_size=batch_size, shuffle=False, num_workers=1, drop_last=True)
+    num_correct = 0
+    num_all = 0
+    for i, data in enumerate(test_dataloader, 0):
+        test_img, test_label = data
+        test_img, test_label = test_img.to(device), test_label.to(device)
+        perturbation = pretrained_G(test_img)
+        perturbation = torch.clamp(perturbation, -0.3, 0.3)
+        adv_img = perturbation + test_img
+        adv_img = torch.clamp(adv_img, 0, 1)
+        pred_lab = torch.argmax(target_model(adv_img),1)
+        num_all += len(test_label)
+        num_correct += torch.sum(pred_lab==test_label,0)
+
+    print('MNIST test dataset:')
+    print('num_examples: ', num_all)
+    print('num_correct: ', num_correct.item())
+    print('accuracy of adv imgs in testing set: %f\n'%(num_correct.item()/num_all))
 
